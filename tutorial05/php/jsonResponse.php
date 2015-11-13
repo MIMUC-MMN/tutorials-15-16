490c155ef5e4f81034bffb3f6d6b8543f4d66bf4
Tobi Stockinger <tobi@tobitobi.de>
2015-11-13 11:31:31 +0100

    05 update, json axax example

--- tutorial05/php/jsonResponse.php
+++ tutorial05/php/jsonResponse.php
@@ -1,17 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tobias
- * Date: 11.11.2015
- * Time: 18:35
- */
-
 
 // this is meta information to facilitate parsing on the receiving end.
-header('content-type','application/json');
+header('Content-Type: application/json');
 
 
-$artists = array("The Rolling Stones", "The Beatles", "Red Hot Chili Peppers", "Käptn Peng");
+$artists = array("artists"=>array("The Rolling Stones", "The Beatles", "Red Hot Chili Peppers", "Käptn Peng"));
 
 $jsonArtist = json_encode($artists);
 
--- tutorial05/php/sop.php
+++ tutorial05/php/sop.php
@@ -3,4 +3,9 @@
  * this file will tell the browser that it's okay that the host might mismatch.
  * It uses the CORS (cross origin resource sharing) paradigm.
  * The trick is to add a simple header to the response.
+ * Source: http://enable-cors.org/server_php.html
  */
+
+header("Access-Control-Allow-Origin: *");
+
+echo "You can load me from anywhere!";
