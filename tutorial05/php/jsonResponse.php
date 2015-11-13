a22f463fb994ef7e42cb96a0fddf42f9c53a675b
Tobi Stockinger <tobi@tobitobi.de>
2015-11-13 18:16:40 +0100

    breakout t05, some minor adjustments

--- tutorial05/php/jsonResponse.php
+++ tutorial05/php/jsonResponse.php
@@ -3,8 +3,12 @@
 // this is meta information to facilitate parsing on the receiving end.
 header('Content-Type: application/json');
 
-
-$artists = array("artists"=>array("The Rolling Stones", "The Beatles", "Red Hot Chili Peppers", "Käptn Peng"));
+$artists =
+    array("artists"
+        =>array("The Rolling Stones",
+            "The Beatles",
+            "Red Hot Chili Peppers",
+            "Käptn Peng"));
 
 $jsonArtist = json_encode($artists);
 
